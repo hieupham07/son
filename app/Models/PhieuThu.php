70c2be6ec48178a1a68229a980db965a1d7dfea2
Pham Trung Hieu <hieupt@boai.vn>
2024-03-13 16:29:19 +0700

    ngày 13

--- app/Models/PhieuThu.php
+++ app/Models/PhieuThu.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class PhieuThu extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $table = 'phieu_thus';
+    protected $fillable = [
+        'ma_phieuthu',
+        'khach_hang_id',
+        'goi_id',
+        'tien_thanhtoan',
+        'tien_con',
+        'ghi_chu',
+    ];
+    public function khachhang()
+    {
+        return $this->belongsTo(KhachHang::class, 'khach_hang_id');
+    }
+    public function details()
+    {
+        return $this->hasMany(PhieuThuDetail::class, 'phieu_id');
+    }
+    public function goi_dt()
+    {
+        return $this->hasMany(GoiDieuTri::class, 'goi_id');
+    }
+    public function phieuthu(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
--- database/migrations/2024_03_12_093839_create_phieu_thus_table.php
+++ database/migrations/2024_03_12_093839_create_phieu_thus_table.php
@@ -14,7 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('phieu_thus', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->unsignedInteger('ma_phieuthu')->nullable();
+            $table->unsignedInteger('khach_hang_id')->nullable();
+            $table->foreign('khach_hang_id')->references('id')->on('khach_hangs')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedInteger('goi_id')->nullable();
+            $table->foreign('goi_id')->references('id')->on('goi_dieu_tris')->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('tien_thanhtoan',18,0)->nullable();
+            $table->decimal('tien_con',18,0)->nullable();
+            $table->text('ghi_chu')->nullable();
             $table->timestamps();
         });
     }
